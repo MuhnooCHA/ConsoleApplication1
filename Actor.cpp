f1afb0ef5267913551be02f846bed73894018de1
MuhnooCHA <gjsdn1268@naver.com>
2023-07-07 13:39:58 +0900

    Actor 추가

---
+++ Actor.cpp
@@ -0,0 +1,21 @@
+#include "Actor.h"
+
+AActor::AActor()
+{
+}
+
+AActor::~AActor()
+{
+}
+
+void AActor::Input()
+{
+}
+
+void AActor::Process()
+{
+}
+
+void AActor::Render()
+{
+}
---
+++ Actor.h
@@ -0,0 +1,17 @@
+#pragma once
+class AActor
+{
+public:
+	AActor();
+	virtual ~AActor();
+
+	int X;
+	int Y;
+	char Shape;
+
+	void Input();
+	void Process();
+	void Render();
+
+};
+
